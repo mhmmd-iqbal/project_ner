03d105d968b9ab61d30ea8def815fa9d3ecfbe99
mhmmd-iqbal <mhmmd.iqbal.june@gmail.com>
2021-08-29 22:59:31 +0700

    update assent direct link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,13 +10,13 @@
                     <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                     <!-- Dark Logo icon -->
                     <img
-                        src="{!! asset('template/assets/images/logo-icon.png') !!}"
+                        src="{{URL::asset('template/assets/images/logo-icon.png')}}"
                         alt="homepage"
                         class="dark-logo"
                     />
                     <!-- Light Logo icon -->
                     <img
-                        src="{!! asset('template/assets/images/logo-icon.png') !!}"
+                        src="{{URL::asset('template/assets/images/logo-icon.png')}}"
                         alt="homepage"
                         class="light-logo"
                     />
@@ -26,13 +26,13 @@
                 <span class="logo-text">
                     <!-- dark Logo text -->
                     <img
-                        src="{!! asset('template/assets/images/logo-text.png') !!}"
+                        src="{{URL::asset('template/assets/images/logo-text.png')}}"
                         alt="homepage"
                         class="dark-logo"
                     />
                     <!-- Light Logo text -->
                     <img
-                        src="{!! asset('template/assets/images/logo-light-text.png') !!}"
+                        src="{{URL::asset('template/assets/images/logo-light-text.png')}}"
                         class="light-logo"
                         alt="homepage"
                     />
@@ -108,7 +108,7 @@
                         aria-expanded="false"
                     >
                         <img
-                            src="{!! asset('template/assets/images/users/1.jpg') !!}"
+                            src="{{URL::asset('template/assets/images/users/1.jpg')}}"
                             alt="user"
                             class="rounded-circle"
                             width="31"
--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,15 +1,15 @@
 <!-- ============================================================== -->
 <!-- All Jquery -->
 <!-- ============================================================== -->
-<script src="{!! asset('template/assets/libs/jquery/dist/jquery.min.js') !!}"></script>
+<script src="{{URL::asset('template/assets/libs/jquery/dist/jquery.min.js') }}"></script>
 <!-- Bootstrap tether Core JavaScript -->
-<script src="{!! asset('template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') !!}"></script>
-<script src="{!! asset('template/dist/js/app-style-switcher.js') !!}"></script>
+<script src="{{URL::asset('template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{URL::asset('template/dist/js/app-style-switcher.js') }}"></script>
 <!--Wave Effects -->
-<script src="{!! asset('template/dist/js/waves.js') !!}"></script>
+<script src="{{URL::asset('template/dist/js/waves.js') }}"></script>
 <!--Menu sidebar -->
-<script src="{!! asset('template/dist/js/sidebarmenu.js') !!}"></script>
+<script src="{{URL::asset('template/dist/js/sidebarmenu.js') }}"></script>
 <!--Custom JavaScript -->
-<script src="{!! asset('template/dist/js/custom.js') !!}"></script>
+<script src="{{URL::asset('template/dist/js/custom.js') }}"></script>
 
 @yield('scripts')
